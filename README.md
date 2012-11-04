c68e406ae1d0d84e1bb6c1a13aea2499d739d0c2
Darren Long <darren.long@mac.com>
2012-11-04 15:21:50 +0000

    Added a Frequency control box (entering 0 queries the KX3's frequency).

--- README.md
+++ README.md
@@ -5,12 +5,16 @@ A trivial gnuradio project for use with the Elecraft KX3, under the GPLv3.
 
 It works.  Just about.  At the moment it doesn't track the VFO on the radio.  It
 can, but there's a memory leak of about 1MB per second when it does, which is
-very bad. In the short-term I'm going to add a button to sync with the VFO on
-demand until I can resolve the problem with the polling method.
+very bad.
+
+I've added a Frequency input control that will retune the radio.  If you enter 0
+in the Frequency control, it will fetch the frequency from the KX3 (as a work
+around for the inability to poll the radio without causing the nasty memory
+leak).
 
 You can click in the waterfall (not in the spectrum plot) to re-tune the radio 
 to that frequency.
 
 Regarding frequency offsets from PBT settings and IF offset configurations, all
 bets are off for now, until the support for the FI command is added to the
-KX3.
+KX3 (which has been promised by Wayne from Elecraft).
--- gr-kx3.py
+++ gr-kx3.py
@@ -2,7 +2,7 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: Kx3 Rx
-# Generated: Sun Oct 28 15:31:01 2012
+# Generated: Sun Nov  4 12:46:22 2012
 ##################################################
 
 '''
@@ -24,10 +24,6 @@ along with gr-kx3.  If not, see <http://www.gnu.org/licenses/>.
 Copyright 2012 Darren Long darren.long@mac.com
 '''
 
-# DML
-gui_scale = 1
-rig_poll_rate = 10
-
 from datetime import datetime
 from gnuradio import audio
 from gnuradio import eng_notation
@@ -36,13 +32,17 @@ from gnuradio import window
 from gnuradio.eng_option import eng_option
 from gnuradio.gr import firdes
 from gnuradio.wxgui import fftsink2
+from gnuradio.wxgui import forms
 from gnuradio.wxgui import waterfallsink2
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
+import pexpect
 import threading
 import time
 import wx
-import pexpect
+
+gui_scale = 2
+rig_poll_rate = 10
 
 class grkx3(grc_wxgui.top_block_gui):
 
@@ -54,11 +54,11 @@ class grkx3(grc_wxgui.top_block_gui):
 		##################################################
 		# Variables
 		##################################################
-		self.prefix = prefix = "~/grdata"
-		self.poll_vfo = poll_vfo = 0
-		self.samp_rate = samp_rate = 48000
 		self.rig_freq = rig_freq = float(pexpect.run("rigctl -m 2 f"))
+		self.prefix = prefix = "/home/darren/grdata"
+		self.samp_rate = samp_rate = 48000
 		self.recfile = recfile = prefix + datetime.now().strftime("%Y.%m.%d.%H.%M.%S") + ".dat"
+		self.freq = freq = rig_freq
 		self.click_freq = click_freq = 0
 
 		##################################################
@@ -71,8 +71,8 @@ class grkx3(grc_wxgui.top_block_gui):
 		self.wxgui_waterfallsink2_0 = waterfallsink2.waterfall_sink_c(
 			self.nb0.GetPage(0).GetWin(),
 			baseband_freq=rig_freq,
-			dynamic_range=30,
-			ref_level=-70,
+			dynamic_range=60,
+			ref_level=-10,
 			ref_scale=1.0,
 			sample_rate=samp_rate,
 			fft_size=2048,
@@ -106,6 +106,17 @@ class grkx3(grc_wxgui.top_block_gui):
 			size=(1190/gui_scale,600/gui_scale),
 		)
 		self.nb0.GetPage(1).Add(self.wxgui_fftsink2_0.win)
+		self.gr_float_to_complex_0 = gr.float_to_complex(1)
+		self._freq_text_box = forms.text_box(
+			parent=self.GetWin(),
+			value=self.freq,
+			callback=self.set_text_freq,
+			label="Frequency",
+			converter=forms.float_converter(),
+		)
+		self.GridAdd(self._freq_text_box, 0, 0, 2, 2)
+		self.audio_source_0 = audio.source(samp_rate, "hw:2,0", True)
+		
 		def _poll_vfo_probe():
 		    prompt="Rig command: "
 		    rigctl = pexpect.spawn("rigctl -m 2")
@@ -126,8 +137,6 @@ class grkx3(grc_wxgui.top_block_gui):
 		_poll_vfo_thread = threading.Thread(target=_poll_vfo_probe)
 		_poll_vfo_thread.daemon = True
 		#_poll_vfo_thread.start()
-		self.gr_float_to_complex_0 = gr.float_to_complex(1)
-		self.audio_source_0 = audio.source(samp_rate, "", True)
 
 		##################################################
 		# Connections
@@ -137,6 +146,17 @@ class grkx3(grc_wxgui.top_block_gui):
 		self.connect((self.audio_source_0, 0), (self.gr_float_to_complex_0, 1))
 		self.connect((self.gr_float_to_complex_0, 0), (self.wxgui_fftsink2_0, 0))
 
+	def get_rig_freq(self):
+		return self.rig_freq
+
+	def set_rig_freq(self, rig_freq):
+		self.rig_freq = rig_freq
+		#self.set_freq(self.rig_freq)
+		print"* set_baseband_freq(" + str(self.rig_freq) + ")"
+		self.wxgui_waterfallsink2_0.set_baseband_freq(self.rig_freq)
+		self.wxgui_fftsink2_0.set_baseband_freq(self.rig_freq)
+		self._freq_text_box.set_value(self.rig_freq)
+
 	def get_prefix(self):
 		return self.prefix
 
@@ -144,35 +164,35 @@ class grkx3(grc_wxgui.top_block_gui):
 		self.prefix = prefix
 		self.set_recfile(self.prefix + datetime.now().strftime("%Y.%m.%d.%H.%M.%S") + ".dat")
 
-	def get_poll_vfo(self):
-		return self.poll_vfo
-
-	def set_poll_vfo(self, poll_vfo):
-		self.poll_vfo = poll_vfo
-		self.set_rig_freq(self.poll_vfo)
-
 	def get_samp_rate(self):
 		return self.samp_rate
 
 	def set_samp_rate(self, samp_rate):
 		self.samp_rate = samp_rate
-		self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
 		self.wxgui_waterfallsink2_0.set_sample_rate(self.samp_rate)
-
-	def get_rig_freq(self):
-		return self.rig_freq
-
-	def set_rig_freq(self, rig_freq):
-		self.rig_freq = rig_freq
-		print"* set_rig_freq(" + str(self.rig_freq) + ")"
-		self.wxgui_waterfallsink2_0.set_baseband_freq(self.rig_freq)
-		self.wxgui_fftsink2_0.set_baseband_freq(self.rig_freq)
+		self.wxgui_fftsink2_0.set_sample_rate(self.samp_rate)
 
 	def get_recfile(self):
 		return self.recfile
 
 	def set_recfile(self, recfile):
 		self.recfile = recfile
+
+	def get_freq(self):
+		return self.freq
+
+	def set_text_freq(self, freq):
+	    if (freq == 0):
+	        got_freq = float(pexpect.run("rigctl -m 2 f"))
+	        print "* got_freq(" + str(got_freq) + ")"
+	        self.freq = got_freq
+	    else:
+	        self.freq = freq
+	        print "* set_freq(" + str(freq) + ")"
+	        result = pexpect.run("rigctl -m 2 F " + str(freq))
+	        print result
+	    self.set_rig_freq(self.freq)
+		
 
 	def get_click_freq(self):
 		return self.click_freq
